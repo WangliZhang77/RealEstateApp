e355b8ec492163082d80d70e4bc05b9dab8b845c
wz219 <wz219@students.waikato.ac.nz>
2026-02-19 22:02:02 +1300

    添加项目文件。

---
+++ RealEstateApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealEstateApp.Models;
+
+
+
+namespace RealEstateApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
+            ): base (options)
+        {
+
+        }
+        public DbSet<ListingCase> ListingCases { get; set; }
+    }
+}
---
+++ RealEstateApp/Models/ListingCase.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+
+namespace RealEstateApp.Models
+{
+    public class ListingCase
+    {
+    public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
